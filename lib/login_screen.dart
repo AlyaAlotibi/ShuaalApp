779aef3f1692f7c74b862861a9189385de569ce3
unknown <AlyaAlotibi@outlook.sa>
2022-05-10 10:41:40 +0300

    add firebase to login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shuaalapp/signup_screen.dart';
 //import 'package:login_ui_design/signup_screen.dart';
-
+import 'package:shuaalapp/home_screen.dart';
 class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => StartState();
+
 }
 
 class StartState extends State<LoginScreen> {
@@ -17,7 +20,8 @@ class StartState extends State<LoginScreen> {
 final _formKey=GlobalKey<FormState>();
   final TextEditingController emailController=new TextEditingController();
   final TextEditingController passwordController=new TextEditingController();
-
+  //Firebase
+  final _auth =FirebaseAuth.instance;
   initWidget() {
     return Scaffold(
         body: SingleChildScrollView(
@@ -63,79 +67,108 @@ final _formKey=GlobalKey<FormState>();
                         )
                     ),
                   ),
+                  Form(
+                      key: _formKey,
+                      child: Column(children: [
+                        Container(
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          height: 54,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.grey[200],
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 10),
+                                  blurRadius: 50,
+                                  color: Color(0xffEEEEEE)
+                              ),
+                            ],
+                          ),
 
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-                    padding: EdgeInsets.only(left: 20, right: 20),
-                    height: 54,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.grey[200],
-                      boxShadow: [
-                        BoxShadow(
-                            offset: Offset(0, 10),
-                            blurRadius: 50,
-                            color: Color(0xffEEEEEE)
-                        ),
-                      ],
-                    ),
-                    child: TextField(
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      onSubmitted: (String value){
-                         emailController.text=value;
-                      },
-                      cursorColor: Color(0xffF5591F),
-                      decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.email,
-                          color: Color(0xffF5591F),
-                        ),
-                        hintText: "Enter Email",
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
-                    ),
+                          child: TextFormField(
+                            controller: emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            validator:(value){
+                              if(value!.isEmpty)
+                              {
+                                return ("Please inter your Email");
+                              }
+                              if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)){
+                                return ("Please inter valid Email");
+                              }
+                              return null;
+                            },
+                            onSaved: (value){
+                              emailController.text=value!;
+                            },
+                            cursorColor: Color(0xffF5591F),
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.email,
+                                color: Color(0xffF5591F),
+                              ),
+                              hintText: "Enter Email",
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                          ),
 
 
-                  ),
+                        ),
 
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                    padding: EdgeInsets.only(left: 20, right: 20),
-                    height: 54,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Color(0xffEEEEEE),
-                      boxShadow: [
-                        BoxShadow(
-                            offset: Offset(0, 20),
-                            blurRadius: 100,
-                            color: Color(0xffEEEEEE)
+                        Container(
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          height: 54,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Color(0xffEEEEEE),
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 20),
+                                  blurRadius: 100,
+                                  color: Color(0xffEEEEEE)
+                              ),
+                            ],
+                          ),
+                          child: TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            validator:(value){
+                              RegExp regex=new RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+                              if(value!.isEmpty)
+                              {
+                                return ("Please inter your Password");
+                              }
+                              if(!regex.hasMatch(value)){
+                                return ("Please inter valid Password at least one \n (upper case,lower case,digit,Special character)\n and at least 8 characters in length");
+                              }
+                              return null;
+                            },
+                            onChanged:  (String value) {
+                              passwordController.text= value;
+                            },
+                            onSaved: (value){
+                              passwordController.text=value!;
+                            },
+                            cursorColor: Color(0xffF5591F),
+                            decoration: InputDecoration(
+                              focusColor: Color(0xffF5591F),
+                              icon: Icon(
+                                Icons.vpn_key,
+                                color: Color(0xffF5591F),
+                              ),
+                              hintText: "Enter Password",
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                    child: TextField(
-                      controller: passwordController,
-                      obscureText: true,
-                      onSubmitted: (String value){
-                        passwordController.text=value;
-                      },
-                      cursorColor: Color(0xffF5591F),
-                      decoration: InputDecoration(
-                        focusColor: Color(0xffF5591F),
-                        icon: Icon(
-                          Icons.vpn_key,
-                          color: Color(0xffF5591F),
-                        ),
-                        hintText: "Enter Password",
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
-                    ),
-                  ),
+                      ],)),
+
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
                     alignment: Alignment.centerRight,
@@ -151,32 +184,44 @@ final _formKey=GlobalKey<FormState>();
                     onTap: () {
                       // Write Click Listener Code Here.
                     },
-                    child: Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-                      padding: EdgeInsets.only(left: 20, right: 20),
-                      height: 54,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(colors: [(new  Color(0xffF5591F)), new Color(0xffF2861E)],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight
-                        ),
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.grey[200],
-                        boxShadow: [
-                          BoxShadow(
-                              offset: Offset(0, 10),
-                              blurRadius: 50,
-                              color: Color(0xffEEEEEE)
+                    child: MaterialButton(
+                      child: Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+                        padding: EdgeInsets.only(left: 20, right: 20),
+                        height: 54,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [(new  Color(0xffF5591F)), new Color(0xffF2861E)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight
                           ),
-                        ],
-                      ),
-                      child: Text(
-                        "LOGIN",
-                        style: TextStyle(
-                            color: Colors.white
+
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 50,
+                                color: Color(0xffEEEEEE)
+                            ),
+                          ],
+                        ),
+                        child: Text(
+                          "LOGIN",
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
                         ),
                       ),
+                      onPressed: (){
+                        if (_formKey.currentState!.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          print(passwordController.text);
+                          signIn(emailController.text, passwordController.text);
+                        }
+
+                      },
                     ),
                   ),
                   Container(
@@ -210,5 +255,16 @@ final _formKey=GlobalKey<FormState>();
             )
         )
     );
+  }
+  void signIn(String email, String password) async{
+    if(_formKey.currentState!.validate()){
+      await _auth.signInWithEmailAndPassword(email: email, password: password).then((value) => {
+        Fluttertoast.showToast(msg: "Login Successful"),
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>HomePageWidget()))
+      }).catchError((e){
+        Fluttertoast.showToast(msg: e!.message);
+      });
+      
+    }
   }
 }
