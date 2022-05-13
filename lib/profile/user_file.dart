bfa477b37620c30524640ac16e56ce479f436f98
ghala <ghalaa.kaj@gmail.com>
2022-05-13 03:39:28 +0300

    profile_user

---
+++ lib/profile/user_file.dart
@@ -0,0 +1,20 @@
+class user{
+  final String imagePath;
+  final String name;
+  final String major;
+  final String about;
+  final bool isDarkMode;
+
+  const user ({
+    required this.imagePath,
+    required this.name,
+    required this.major,
+    required this.about,
+    required this.isDarkMode,
+  });
+
+
+
+
+
+}
---
+++ lib/profile/user_pref.dart
@@ -0,0 +1,16 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shuaalapp/profile/user_file.dart';
+import 'package:shuaalapp/profile/user_profile.dart';
+
+
+class UserPre{
+static const  myUser= user(
+  imagePath:'',
+  name:'',
+  about:'',
+  major:'',
+    isDarkMode:false,
+);
+
+}
